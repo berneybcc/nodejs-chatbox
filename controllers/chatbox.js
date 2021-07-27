990d472c789578b27f05a4df5a58acd8abb20027
berneybcc <57729351+berneybcc@users.noreply.github.com>
2021-07-27 14:46:24 -0500

    Se instala cors

--- controllers/chatbox.js
+++ controllers/chatbox.js
@@ -1,15 +1,106 @@
+const { restart } = require('nodemon');
 var QuestionsModel = require('../models/questions');
 
-function adminQuestion(req,res){
-    res.render('adminQuestions');
+async function adminQuestion(req,res){
+    var data = await obtainQuestion([]);
+    if(!data.error){
+        data = data.data;
+        console.log("Consulta Sub Questions");
+        // var subData= await createArrayQuestion(data);
+    }
+    // console.log(subData);
+    // res.send(subData);
+    res.render('adminQuestions',{info:data});
 }
-function obtainQuestion(req, res){ 
-    QuestionsModel.find(function(err,question){
-        question.forEach((data)=>{
-            console.log(data.id);
-        })
-        res.send(question);
-    })
+
+function testButton(){
+    console.log("butom");
+}
+
+async function createArrayQuestion(data){
+    var returnDatos=[];
+    for(var i=0;i<data.length;i++){
+        var uid= data[i].id;
+        var datosObject={};
+        datosObject[uid]=[];
+        datosObject.id=uid;
+        datosObject.description = data[i].description;
+        var infoOne = await obtainQuestion([uid]);
+        if(!infoOne.error){
+            var oneLevel= infoOne.data;            
+            for(var a=0;a<oneLevel.length;a++){
+                var uidOne= oneLevel[a].id;
+                var datosOneObject={};
+                datosOneObject[uidOne]=[];
+                datosOneObject.id=uidOne;
+                datosOneObject.description = oneLevel[a].description;
+                datosObject[uid].push(datosOneObject);
+                var infoTwo = await obtainQuestion([uid,uidOne]);
+                if(!infoTwo.error){
+                    datosObject[uid][a][uidOne].push(infoTwo.data);
+                }
+            }
+        }
+        // var info = await obtainQuestionLikeRelation(uid);
+        // if(!info.error){
+        //     var subData = info.data;
+        //     for(var a=0;a<subData.length;a++){
+        //         var uidSub=subData[a].id;
+        //         var datosSub={};
+        //         datosSub.id=uidSub;
+        //         datosSub.description=subData[a].description;
+        //         var relation =subData[a].relation;
+        //         uid_end= relation[relation.length-1];
+        //         if(typeof datosObject[uid_end]!== 'undefined'){
+        //             datosObject[uid_end].push(datosSub);
+        //         }
+        //         else{
+        //             if(datosObject[uid].length > 0){
+        //                 for(var c=0;c<datosObject[uid].length;c++){
+        //                     if(relation.indexOf(datosObject[uid][c].id)>=0){
+        //                         if(typeof datosObject[uid][c][datosObject[uid][c].id] === 'undefined'){
+        //                             datosObject[uid][c][datosObject[uid][c].id]=[];
+        //                         }                                
+        //                         datosObject[uid][c][datosObject[uid][c].id].push(datosSub);
+        //                     };
+        //                 }
+        //             }
+        //         }                
+        //     }
+        // }
+        returnDatos.push(datosObject);
+    }
+    return returnDatos;
+}
+
+function obtainQuestionLikeRelation(id){
+    return new Promise((resutl) => {QuestionsModel.find({relation:{ $regex: '.*' + id + '.*' }},function(error,data){
+        var msg = {
+            error:false,
+            data:data
+        }
+        if(error || data.length === 0){
+            msg.error = true;
+            msg.data = "No existe Registro para el id enviado";
+            // console.log(msg);
+        }
+        resutl(msg);
+    })});
+}
+
+function obtainQuestion(valueRelation){
+    return new Promise((resutl) => {QuestionsModel.find({relation:valueRelation},function(error,data){
+        var msg = {
+            error:false,
+            data:data
+        }
+        if(error || data.length === 0){
+            console.log(error);
+            msg.error = true;
+            msg.data = "No existe Registro para el id enviado";
+        }
+        resutl(msg);
+    })});
 }
 
 function obtainOneQuestion(id){
@@ -17,7 +108,7 @@ function obtainOneQuestion(id){
         var msg= {
             error:false,
         };
-        if(error){
+        if(error || !question){
             msg.error = true;
             msg.data="No existe registro";
         }else{
@@ -30,37 +121,48 @@ function obtainOneQuestion(id){
 async function obtainRelationQuestion(req,res){
     var ids = req.params.ids;
     var valueRelation = [];
+    console.log(ids);
     if (typeof ids !== "undefined") {
-        var dataRelation = this.obtainRelation(ids);
+        var dataRelation = await obtainRelation(ids);
         valueRelation = (!dataRelation.error)?dataRelation.data:[];
-        console.log(valueRelation);
     }
-    QuestionsModel.find({relation:valueRelation},function(error,data){
-        var msg = {
-            error:false,
-            data:data
-        }
-        if(error || data.length === 0){
-            console.log(error);
-            msg.error = true;
-            msg.data = "No existe Registro para el id enviado";
-        }
-        res.send(msg);
-    });
+    console.log("Valor Array: "+valueRelation);
+    var info =await obtainQuestion(valueRelation);
+    console.log(info);
+    res.send(info);
 }
 
-function saveQuestion(req,res){
+async function saveQuestion(req,res){
+    console.log('Ingresa Save');
+    var returnInfo={
+        error:false,
+        data:"Datos registrado"
+    };
     var data = req.body;
-    console.log(data);
     var valueDescription = data.inputDescripcion;
     var valueRelation = [];
-    if(typeof data.flexRadioDefault !== "undefined"){
-        var dataRelation = this.obtainRelation(ids);
-        valueRelation = (!dataRelation.error)?dataRelation.data:[];
+    if(typeof data.checkQuestions !== "undefined"){
+        console.log(data.checkQuestions);
+        var dataRelation =await obtainRelation(data.checkQuestions);
+        if(!dataRelation.error){
+            console.log("DataRelation: "+dataRelation);
+            valueRelation = (!dataRelation.error)?dataRelation.data:[];
+            console.log(valueRelation);
+        }else{
+            returnInfo.error = true;
+            returnInfo.data = "No existe la Pregunta asociada";
+            return res.send(returnInfo);
+        }
     }
+    if(!valueDescription){
+        returnInfo.error = true;
+        returnInfo.data = "Se requiere una Descripcion";
+        return res.send(returnInfo);
+    }
+
     var questionsQuery= new QuestionsModel({description:valueDescription,relation:valueRelation});
-    questionsQuery.save();
-    return res.send("Dato insertado");
+    await questionsQuery.save();
+    return res.send(returnInfo);
 }
 
 async function obtainRelation(ids){
@@ -70,6 +172,7 @@ async function obtainRelation(ids){
         console.log(infoSearch);
         var data = infoSearch.data;
         var idInfo = data.id;
+        console.log("Id: "+idInfo);
         var relationInfo = data.relation;
         relationInfo.push(idInfo);
         dataReturn.error = false;
@@ -79,7 +182,6 @@ async function obtainRelation(ids){
 } 
 
 module.exports={
-    obtainQuestion,
     saveQuestion,
     obtainOneQuestion,
     obtainRelationQuestion,
--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors')
 var ChatboxController = require('../controllers/chatbox');
 
-router.post('/questions/save', ChatboxController.saveQuestion);
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+router.post('/questions/save',cors(corsOptions), ChatboxController.saveQuestion);
 router.get('/questions/relation',ChatboxController.obtainRelationQuestion);
-router.get('/questions/obtain/:ids?',ChatboxController.obtainRelationQuestion);
+router.get('/questions/obtain/:ids?',cors(corsOptions),ChatboxController.obtainRelationQuestion);
 router.get('/admin/questions',ChatboxController.adminQuestion);
 
 /* GET users listing. */
