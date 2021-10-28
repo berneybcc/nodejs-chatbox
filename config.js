238a3a7c1e97a0c7b9a0e20ac363eb83d730176a
berneybcc <57729351+berneybcc@users.noreply.github.com>
2021-10-28 18:57:48 -0500

    Varibles de entorno

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 module.exports={
-    TOKEN_FACEBOOK :"EAAIXyZAA7ZCsQBACdiZAFjHyEi1v4G0hjsBjnkwNZChkdKdRDtlqc40ahZA6mZBtYZAXC04rAZAJUZBqm84qV0evMhgaRcgdyhvXmSIMdxxV5YGmsjVjXoYC8Kwt8ZArohJ11xOPGZAz9PIL5TLTQAXWkUbwh8RVeDBVRdUZArRAayygcHcPVpSBiTVWHiknhsWxg2YZD",
-    URI_FACEBOOK:"https://graph.facebook.com/v11.0/me/messages"
+    TOKEN_FACEBOOK :"EAAIXyZAA7ZCsQBALdvZCdDYEAZB7WqRZC8ZBAX6EnXlmdiZBE7HAhXTP6ZCNY8bbJ2s9lu3LtDA3vBC7Lf2JEEZCsTsnJsjmZBZClhJIzaoJzsv5V3MXgktqqFVAp5VsjRlNnqMGoVdGqkuGFJ7CsZBC4uXIinCZA61aUhvEJWqR90qZCi7eZAF9fnvqLvH2dh7rxI9OSgZD",
+    URI_FACEBOOK:"https://graph.facebook.com/v11.0/me/messages",
+    MEG_WELCOME:"Bienvenido",
+    MEG_SELECCION:"Seleccione una opcion",
+    SUB_MEG:"Por favor elija la opcion deseada"
 }
